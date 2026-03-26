1739229c3a34dfd80dda0fb8a7f11b02fa34e2f3
angelprivado2801 <angelprivado2801@gmail.com>
2026-03-25 19:47:52 -0500

    PRACTICA3.py

--- CORTE 1/Practica 3/PRACTICA3.py
+++ CORTE 1/Practica 3/PRACTICA3.py
@@ -1,1 +1,12 @@
+while True:
+    a = input("Por favor ingrese un valor")
+    aInt = int(a)
+    mod = aInt%2
 
+    if(mod == 0):
+        print("a es un PAR")
+    else:
+        print("a NO es PAR, es IMPAR")
+    salir = input("¿Desea Finalizar? Si=s, No=n").lower()
+    if salir == "s":
+        break
