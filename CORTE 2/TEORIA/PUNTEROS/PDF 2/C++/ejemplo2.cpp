5facec59259979d2495ebf141115d3c8a69c80dc
angelprivado2801 <angelprivado2801@gmail.com>
2026-05-24 13:58:38 -0500

    ejemplo2.cpp

---
+++ CORTE 2/TEORIA/PUNTEROS/PDF 2/C++/ejemplo2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int x = 10;
+    
+    std::cout << "Valor de x = " << x << "\n";
+    std::cout << "Direccion de x = " << &x << "\n";
+    
+    return 0;
+}
