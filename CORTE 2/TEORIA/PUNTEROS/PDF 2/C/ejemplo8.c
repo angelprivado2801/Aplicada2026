500a582b6174190b66a392ede66857eeacec07cb
angelprivado2801 <angelprivado2801@gmail.com>
2026-05-24 14:13:16 -0500

    ejemplo8.c

---
+++ CORTE 2/TEORIA/PUNTEROS/PDF 2/C/ejemplo8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void cambiar(int n) {
+    n = 100;
+    printf("Dentro de cambiar: n = %d\n", n);
+}
+
+int main(void) {
+    int x = 5;
+    
+    printf("Antes: x = %d\n", x);
+    cambiar(x);
+    printf("Despues: x = %d\n", x); // x no cambia
+    
+    return 0;
+}
