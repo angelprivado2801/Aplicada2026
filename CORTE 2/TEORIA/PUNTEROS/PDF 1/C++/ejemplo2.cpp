abc0af810379485cc172855bed6ad9c38c14d95d
angelprivado2801 <angelprivado2801@gmail.com>
2026-05-24 13:38:11 -0500

    Update ejemplo2.cpp
    Updated the intercambiar function to use nullptr for null checks.

--- CORTE 2/TEORIA/PUNTEROS/PDF 1/C++/ejemplo2.cpp
+++ CORTE 2/TEORIA/PUNTEROS/PDF 1/C++/ejemplo2.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+// En C++, se prefiere usar nullptr en lugar de NULL
+void intercambiar(int *a, int *b) {
+    if (a == nullptr || b == nullptr) {
+        return;
+    }
+    int temp = *a;
+    
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    std::cout << "Antes: x = " << x << ", y = " << y << std::endl;
+    
+    intercambiar(&x, &y);
+    
+    std::cout << "Despues: x = " << x << ", y = " << y << std::endl;
+    
+    return 0;
+}
