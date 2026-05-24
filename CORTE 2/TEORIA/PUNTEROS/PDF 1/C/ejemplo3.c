cbe565d136ffab4606f0bf7926f6ca2a294f0931
angelprivado2801 <angelprivado2801@gmail.com>
2026-05-24 13:46:09 -0500

    ejemplo3.c

---
+++ CORTE 2/TEORIA/PUNTEROS/PDF 1/C/ejemplo3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int x = 25;
+    int *p = &x;
+
+    printf("x = %d\n", x);
+    printf("&x 0 %p\n", (void *)&x);
+    printf("p = %p\n", (void *)p);
+    printf("*p = %d\n", *p);
+
+    return 0;
+}
