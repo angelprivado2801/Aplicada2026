8dc76e2e7c0d28c70fcb4c15018d1a808a826c67
angelprivado2801 <angelprivado2801@gmail.com>
2026-05-24 13:38:57 -0500

    Update ejemplo3.cpp
    Added pointer demonstration in ejemplo3.cpp.

--- CORTE 2/TEORIA/PUNTEROS/PDF 1/C++/ejemplo3.cpp
+++ CORTE 2/TEORIA/PUNTEROS/PDF 1/C++/ejemplo3.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+int main() {
+    int x = 25;
+    int *p = &x;
+
+    std::cout << "x = " << x << std::endl;
+    std::cout << "&x = " << (void *)&x << std::endl;
+    std::cout << "p = " << (void *)p << std::endl;
+    std::cout << "*p = " << *p << std::endl;
+
+    return 0;
+}
